6eb064c6be389e960751513cb1e6d8601e45a0df
Nalien <211351701+NalienDev@users.noreply.github.com>
2026-04-11 22:35:21 +0100

    Add CoolWeatherApp Android project

---
+++ tutorial-2/android/coolweatherapp/app/src/main/java/dam/a51793/coolweatherapp/MainActivity.kt
@@ -0,0 +1,42 @@
+package dam.a51793.coolweatherapp
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        val day = true //TODO this is determined by the current time and sunrise/sunset hours
+
+        when (resources.configuration.orientation) {
+            Configuration.ORIENTATION_PORTRAIT -> {
+                if (day) {
+                    setTheme(R.style.Theme_Day)
+                } else {
+                    setTheme(R.style.Theme_Night)
+                }
+            }
+
+            Configuration.ORIENTATION_LANDSCAPE -> {
+                if (day) {
+                    setTheme(R.style.Theme_Day_Land)
+                } else {
+                    setTheme(R.style.Theme_Night_Land)
+                }
+            }
+        }
+
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
