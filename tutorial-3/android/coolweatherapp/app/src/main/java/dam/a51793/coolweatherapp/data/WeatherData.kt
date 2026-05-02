80328e4526307a41b01725f767e92238453ed58b
Nalien <211351701+NalienDev@users.noreply.github.com>
2026-05-02 16:23:01 +0100

    Add initial Cool Jetpack Weather App code

---
+++ tutorial-3/android/coolweatherapp/app/src/main/java/dam/a51793/coolweatherapp/data/WeatherData.kt
@@ -0,0 +1,38 @@
+package dam.a51793.coolweatherapp.data
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class WeatherData(
+    var latitude: String,
+    var longitude: String,
+    var timezone: String,
+    var current_weather: CurrentWeather,
+    var hourly: Hourly,
+    var daily: Daily
+)
+
+data class Daily(
+    var sunrise: ArrayList<String>,
+    var sunset: ArrayList<String>
+)
+data class CurrentWeather(
+    var temperature: Float,
+    var windspeed: Float,
+    var winddirection: Int,
+    var weathercode: Int,
+    var time: String
+)
+
+data class Hourly(
+    var time: ArrayList<String>,
+    var temperature_2m: ArrayList<Float>,
+    var weathercode: ArrayList<Int>,
+    var pressure_msl: ArrayList<Double>
+)
+
+data class WeatherCodeInfo(
+    val code: Int,
+    val description: String,
+    val image: String
+)
